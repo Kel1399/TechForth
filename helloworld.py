cfaaac5feae3afd3063257d49ed55d20ea6976c2
Kel1399 <kellynpintado23@gmail.com>
2024-11-22 16:45:42 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+"""
+name = "Kelly"
+age = 13
+funFact = "I whent to Ecuador with only my sister."
+print(name + str(age) + funFact)
+"""
+"""
+username = input("What is your name?")
+print("Hallo ich bin " +username)
+
+age = int(input("What is your name"))
+print("you are" + str(age) + "years old")
+"""
+num1 = int(input("num1:"))
+num2 = int(input("num2:"))
+total = num1 + num2
+print(str(total))
